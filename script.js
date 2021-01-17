181fd45d07152521d309e9121a4d999e1b75d31c
balsasc <scekicb02@gmail.com>
2021-01-17 14:10:18 +0100

    Added a button to clear the grid. Allowed users to to input their own grid size.

--- script.js
+++ script.js
@@ -1,17 +1,83 @@
-//31.25
+const mainHeading = document.createElement('h1');
+mainHeading.innerText = "Etch-a-Sketch";
+document.body.prepend(mainHeading);
+
+const main = document.querySelector('.main');
 
 const container = document.querySelector('.container');
+const containerStyle = getComputedStyle(container);
+const containerSize = container.clientHeight;
 
+const optionsContainer = document.createElement('div');
+optionsContainer.classList.add('options-container');
+main.prepend(optionsContainer);
 
-let index = 1;
-while (index <= (16 * 16)) {
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
-    
-    gridElement.addEventListener('mouseover', () => {
-        gridElement.classList.add('change-to-black');
-    })
-    
-    container.append(gridElement);
-    index++;
+const clearButton = document.createElement('button');
+clearButton.innerText = "Clear Grid";
+clearButton.classList.add('btn');
+optionsContainer.prepend(clearButton);
+
+const fullClearGrid = () => {
+    container.innerHTML = '';
 }
+
+const changeSize = document.createElement('input');
+changeSize.setAttribute('type', 'number');
+changeSize.setAttribute('value', '16');
+changeSize.setAttribute('min', '10');
+changeSize.setAttribute('max', '64');
+optionsContainer.prepend(changeSize);
+
+changeSize.addEventListener('change', () => {
+    fullClearGrid();
+    selectedValue = parseInt(changeSize.value);
+    let index = 1;
+    while (index <= selectedValue *  selectedValue) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', () => {
+            gridElement.classList.add('change-to-black');
+        })
+    
+        clearButton.addEventListener('click', () => {
+            gridElement.classList.remove('change-to-black');
+        });
+    
+        container.append(gridElement);
+        index++;
+    }
+    
+    container.style.gridTemplateColumns = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+    container.style.gridTemplateRows = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+})
+
+
+
+
+
+
+
+// const selectedValue = parseInt(changeSize.value); // for now
+
+// let index = 1;
+//     while (index <= selectedValue *  selectedValue) {
+//     const gridElement = document.createElement('div');
+//     gridElement.classList.add('grid-element');
+//     gridElement.addEventListener('mouseover', () => {
+//         gridElement.classList.add('change-to-black');
+//     })
+
+//     clearButton.addEventListener('click', () => {
+//         gridElement.classList.remove('change-to-black');
+//     });
+
+//     container.append(gridElement);
+//     index++;
+// }
+
+// container.style.gridTemplateColumns = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+// container.style.gridTemplateRows = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+
+
+
+
