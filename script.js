0c0c87ac5b86c97997914d12470fb66b187f0134
balsasc <scekicb02@gmail.com>
2021-01-17 15:15:33 +0100

    Allowed users to pick the color of their pen. Add a createGrid() function which creates a default grid and allows users to change its size.

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ main.prepend(optionsContainer);
 const clearButton = document.createElement('button');
 clearButton.innerText = "Clear Grid";
 clearButton.classList.add('btn');
-optionsContainer.prepend(clearButton);
+// optionsContainer.prepend(clearButton);
 
 const fullClearGrid = () => {
     container.innerHTML = '';
@@ -26,29 +26,65 @@ changeSize.setAttribute('type', 'number');
 changeSize.setAttribute('value', '16');
 changeSize.setAttribute('min', '10');
 changeSize.setAttribute('max', '64');
-optionsContainer.prepend(changeSize);
 
-changeSize.addEventListener('change', () => {
-    fullClearGrid();
-    selectedValue = parseInt(changeSize.value);
+const colorPicker = document.createElement('input');
+colorPicker.setAttribute('type', 'color');
+optionsContainer.append(changeSize, colorPicker, clearButton);
+
+const labelSize = document.createElement('label');
+labelSize.setAttribute('for', 'size');
+labelSize.innerText = 'Squares per side: '
+optionsContainer.prepend(labelSize);
+
+// Default grid size is going to be 16x16, but users can change it.
+const createGrid = () => {
+    let selectedValue = parseInt(changeSize.value);
     let index = 1;
     while (index <= selectedValue *  selectedValue) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
-        gridElement.addEventListener('mouseover', () => {
-            gridElement.classList.add('change-to-black');
+        gridElement.addEventListener('mouseenter', (e) => {
+            let randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; // gives a random hex color;
+            gridElement.style.background = `${colorPicker.value}`;
+            // gridElement.classList.add('change-to-black');
         })
     
         clearButton.addEventListener('click', () => {
-            gridElement.classList.remove('change-to-black');
+            gridElement.style.background = `#fff`;
+            // gridElement.classList.remove('change-to-black');
         });
     
         container.append(gridElement);
         index++;
     }
-    
+
     container.style.gridTemplateColumns = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
     container.style.gridTemplateRows = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+}
+createGrid();
+
+changeSize.addEventListener('change', () => {
+    fullClearGrid();
+    createGrid();
+    // selectedValue = parseInt(changeSize.value);
+    // let index = 1;
+    // while (index <= selectedValue *  selectedValue) {
+    //     const gridElement = document.createElement('div');
+    //     gridElement.classList.add('grid-element');
+    //     gridElement.addEventListener('mouseover', () => {
+    //         gridElement.classList.add('change-to-black');
+    //     })
+    
+    //     clearButton.addEventListener('click', () => {
+    //         gridElement.classList.remove('change-to-black');
+    //     });
+    
+    //     container.append(gridElement);
+    //     index++;
+    // }
+    
+    // container.style.gridTemplateColumns = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
+    // container.style.gridTemplateRows = `repeat(${(selectedValue)}, ${containerSize / (selectedValue)}px)`;
 })
 
 
