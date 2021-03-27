7d6cb3348bf78453a02a77651ef684003c1bec68
balsasc <scekicb02@gmail.com>
2021-03-27 12:01:30 +0100

    users can now change the grid size directly from the size input

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const gridContainer = document.querySelector(".grid-wrapper");
 const size = document.querySelector(".size");
 
-containerHeight = 650;
-let height = parseInt(size.value) * parseInt(size.value);
+let width = parseInt(size.value);
+let height = parseInt(size.value);
+let gridSize = 640;
 
+gridContainer.style.height = `${gridSize}px`;
+gridContainer.style.width = `${gridSize}px`;
 
-gridContainer.style.gridTemplateColumns = `repeat(${parseInt(size.value)}, ${containerHeight / parseInt(size.value)}px)`
-  
-for (let i = 0; i < height; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("grid-item");
-  gridItem.style.height = `${containerHeight / parseInt(size.value)}px`;
-  gridItem.style.width = `${containerHeight / parseInt(size.value)}px`;
-  gridContainer.append(gridItem);
+const createGrid = (h, w) => {
+  for (let i = 0; i < h * w; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.style.height = `${gridSize / h}px`;
+    gridItem.style.width = `${gridSize / w}px`;
+    gridItem.style.border = `1px solid #333`;
+    gridContainer.append(gridItem);
+  }
+  gridContainer.style.gridTemplateColumns = `repeat(${parseInt(size.value)}, ${gridSize / h}px)`
 }
 
-const clear = () => {
-  // todo
-}
+createGrid(height, width);
+
+size.addEventListener('change', () => {
+  gridContainer.innerHTML = "";
+  createGrid(parseInt(size.value), parseInt(size.value));
+})
+
